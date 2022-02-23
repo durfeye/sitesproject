c58bb10746bc4ba692cb77b151a1cbc933961f2b
durfeye <94008782+durfeye@users.noreply.github.com>
2022-02-23 15:42:09 +0100

    Add files via upload

--- sendmail.php
+++ sendmail.php
@@ -1,11 +1,20 @@
-<?php 
-$name = $_POST['userName'];
-$phone = $_POST['phoneNumber'];
-$email = $_POST['mail'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n Phone: $phone";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+<?php
+       // from the form
+       $name = trim(strip_tags($_POST['userName']));
+       $number = trim(strip_tags($_POST['phoneNumber']));
+       $email = trim(strip_tags($_POST['mail']));
+       $message = htmlentities($_POST['message']);
+
+       // set here
+       $subject = "Zapytanie z formularza";
+       $to = 'polishl0rd2@gmail.com';
+
+        $body = <<<HTML
+Imię i nazwisko: $name \n E-mail: $email \n Numer telefonu: $number \n Wiadomość: \n $message
+HTML;
+
+       $headers = "Zapytanie z formularza";
+
+       // send the email
+       mail($to, $subject, $body, $headers);
 ?>
